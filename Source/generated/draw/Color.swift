1357c090c5274a1274db86c54cdb4232944a5255
Nikita Afonasov <bistrform@gmail.com>
2015-12-23 15:28:54 +0600

    Static let fields are now not optional

--- Source/generated/draw/Color.swift
+++ Source/generated/draw/Color.swift
@@ -4,22 +4,22 @@ public class Color: Fill  {
 
 	let val: Int
 
-	public static let white: Color? = Color( val: 0xFFFFFF )
-	public static let silver: Color? = Color( val: 0xC0C0C0 )
-	public static let gray: Color? = Color( val: 0x808080 )
-	public static let black: Color? = Color( val: 0 )
-	public static let red: Color? = Color( val: 0xFF0000 )
-	public static let maroon: Color? = Color( val: 0x800000 )
-	public static let yellow: Color? = Color( val: 0xFFFF00 )
-	public static let olive: Color? = Color( val: 0x808000 )
-	public static let lime: Color? = Color( val: 0x00FF00 )
-	public static let green: Color? = Color( val: 0x008000 )
-	public static let aqua: Color? = Color( val: 0x00FFFF )
-	public static let teal: Color? = Color( val: 0x008080 )
-	public static let blue: Color? = Color( val: 0x0000FF )
-	public static let navy: Color? = Color( val: 0x000080 )
-	public static let fuchsia: Color? = Color( val: 0xFF00FF )
-	public static let purple: Color? = Color( val: 0x800080 )
+	public static let white: Color = Color( val: 0xFFFFFF )
+	public static let silver: Color = Color( val: 0xC0C0C0 )
+	public static let gray: Color = Color( val: 0x808080 )
+	public static let black: Color = Color( val: 0 )
+	public static let red: Color = Color( val: 0xFF0000 )
+	public static let maroon: Color = Color( val: 0x800000 )
+	public static let yellow: Color = Color( val: 0xFFFF00 )
+	public static let olive: Color = Color( val: 0x808000 )
+	public static let lime: Color = Color( val: 0x00FF00 )
+	public static let green: Color = Color( val: 0x008000 )
+	public static let aqua: Color = Color( val: 0x00FFFF )
+	public static let teal: Color = Color( val: 0x008080 )
+	public static let blue: Color = Color( val: 0x0000FF )
+	public static let navy: Color = Color( val: 0x000080 )
+	public static let fuchsia: Color = Color( val: 0xFF00FF )
+	public static let purple: Color = Color( val: 0x800080 )
 
 	public init(val: Int = 0) {
 		self.val = val	

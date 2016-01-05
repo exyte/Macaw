fe0dbefe237bea3c0197cd6c6377331d419184a3
Yuri Strot <ystrot@gmail.com>
2016-01-05 20:29:48 +0600

    Resolve #9: Render all Macaw geometry (except for Path)

--- Example/Example/CustomView.swift
+++ Example/Example/CustomView.swift
@@ -13,9 +13,36 @@ import Macaw
 class CustomView: MacawView {
 
     required init?(coder aDecoder: NSCoder) {
-        let rect = Rect(x: 50, y: 50, w: 200, h: 100)
-        let shape = Shape(form: rect, fill: Color.purple)
-        super.init(node: shape, coder: aDecoder)
+        let rect = Rect(x: 50, y: 50, w: 50, h: 100)
+        let shape1 = Shape(form: rect, fill: Color.blue)
+
+        let round = RoundRect(rect: Rect(x: 150, y: 50, w: 50, h: 100), rx: 10, ry: 10)
+        let shape2 = Shape(form: round, fill: Color.navy)
+
+        let circle = Circle(cx: 50, cy: 200, r: 30)
+        let shape3 = Shape(form: circle, fill: Color.purple)
+
+        let ellipse = Ellipse(cx: 150, cy: 200, rx: 50, ry: 30)
+        let shape4 = Shape(form: ellipse, fill: Color.fuchsia)
+
+        let polygon = Polygon(points: [280, 60, 240, 95, 330, 150, 330, 100])
+        let shape5 = Shape(form: polygon, fill: Color.maroon)
+    
+        let polyline = Polyline(points: [260, 150, 220, 185, 310, 240, 310, 190])
+        let shape6 = Shape(form: polyline, stroke: Stroke(fill: Color.olive, width: 5, join: LineJoin.round))
+
+        let line = Line(x1: 30, y1: 290, x2: 130, y2: 290)
+        let shape7 = Shape(form: line, stroke: Stroke(fill: Color.lime, width: 10, cap: LineCap.round))
+
+        let arc = Arc(ellipse: Ellipse(cx: 250, cy: 250, rx: 50, ry: 50), shift: M_PI / 2, extent: M_PI / 2)
+        let shape8 = Shape(form: arc, stroke: Stroke(fill: Color.green, dashes: [3, 3]))
+
+        let point = Point(x: 300, y: 290)
+        let shape9 = Shape(form: point, stroke: Stroke(fill: Color.gray, width: 7, cap: LineCap.round))
+
+        let group = Group(contents: [shape1, shape2, shape3, shape4, shape5, shape6, shape7, shape9, shape8])
+
+        super.init(node: group, coder: aDecoder)
     }
 
     required init?(node: Node, coder aDecoder: NSCoder) {
--- Source/views/MacawView.swift
+++ Source/views/MacawView.swift
@@ -24,34 +24,158 @@ public class MacawView: UIView {
 
     override public func drawRect(rect: CGRect) {
         let ctx = UIGraphicsGetCurrentContext();
+        drawNode(node, ctx: ctx)
+    }
+
+    private func drawNode(node: Node, ctx: CGContext?) {
         if let shape = node as? Shape {
             setGeometry(shape.form!, ctx: ctx)
-            setFill(shape.fill!, ctx: ctx)
+            setFill(shape.fill, ctx: ctx)
+            setStroke(shape.stroke, ctx: ctx)
+        } else if let group = node as? Group {
+            for content in group.contents {
+                drawNode(content, ctx: ctx)
+            }
         } else {
             print("Unsupported node: \(node)")
         }
-
-        CGContextFillPath(ctx);
     }
 
     private func setGeometry(locus: Locus, ctx: CGContext?) {
         if let rect = locus as? Rect {
-            CGContextAddRect(ctx, CGRect(x: CGFloat(rect.x), y: CGFloat(rect.y), width: CGFloat(rect.w), height: CGFloat(rect.h)))
+            CGContextAddRect(ctx, newCGRect(rect))
+        } else if let round = locus as? RoundRect {
+            let corners = CGSizeMake(CGFloat(round.rx), CGFloat(round.ry))
+            let path = UIBezierPath(roundedRect: newCGRect(round.rect!), byRoundingCorners:
+                UIRectCorner.AllCorners, cornerRadii: corners).CGPath
+            CGContextAddPath(ctx, path)
+        } else if let circle = locus as? Circle {
+            let cx = CGFloat(circle.cx)
+            let cy = CGFloat(circle.cy)
+            let r = CGFloat(circle.r)
+            CGContextAddEllipseInRect(ctx, CGRect(x: cx - r, y: cy - r, width: r * 2, height: r * 2))
+        } else if let ellipse = locus as? Ellipse {
+            let cx = CGFloat(ellipse.cx)
+            let cy = CGFloat(ellipse.cy)
+            let rx = CGFloat(ellipse.rx)
+            let ry = CGFloat(ellipse.ry)
+            CGContextAddEllipseInRect(ctx, CGRect(x: cx - rx, y: cy - ry, width: rx * 2, height: ry * 2))
+        } else if let arc = locus as? Arc {
+            CGContextAddPath(ctx, toBezierPath(arc).CGPath)
+        } else if let point = locus as? Point {
+            let path = UIBezierPath()
+            path.moveToPoint(CGPointMake(CGFloat(point.x), CGFloat(point.y)))
+            path.addLineToPoint(CGPointMake(CGFloat(point.x), CGFloat(point.y)))
+            CGContextAddPath(ctx, path.CGPath)
+        } else if let line = locus as? Line {
+            let path = UIBezierPath()
+            path.moveToPoint(CGPointMake(CGFloat(line.x1), CGFloat(line.y1)))
+            path.addLineToPoint(CGPointMake(CGFloat(line.x2), CGFloat(line.y2)))
+            CGContextAddPath(ctx, path.CGPath)
+        } else if let polygon = locus as? Polygon {
+            let path = toBezierPath(polygon.points)
+            path.closePath()
+            CGContextAddPath(ctx, path.CGPath)
+        } else if let polygon = locus as? Polyline {
+            CGContextAddPath(ctx, toBezierPath(polygon.points).CGPath)
         } else {
             print("Unsupported locus: \(locus)")
         }
     }
 
-    private func setFill(fill: Fill, ctx: CGContext?) {
-        if let color = fill as? Color {
-            let red = CGFloat(Double(color.r()) / 255.0);
-            let green = CGFloat(Double(color.g()) / 255.0);
-            let blue = CGFloat(Double(color.b()) / 255.0);
-            let alpha = CGFloat(Double(color.a()) / 255.0);
-            CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
-        } else {
-            print("Unsupported fill: \(fill)")
+    private func toBezierPath(points: [NSNumber]) -> UIBezierPath {
+        let parts = 0.stride(to: points.count, by: 2).map { Array(points[$0..<$0 + 2]) }
+        let path = UIBezierPath()
+        var first = true
+        for part in parts {
+            let point = CGPointMake(CGFloat(part[0]), CGFloat(part[1]))
+            if (first) {
+                path.moveToPoint(point)
+                first = false
+            } else {
+                path.addLineToPoint(point)
+            }
         }
+        return path
+    }
+
+    private func toBezierPath(arc: Arc) -> UIBezierPath {
+        let extent = CGFloat(arc.extent)
+        let end = CGFloat(arc.shift) + extent
+        let ellipse = arc.ellipse!
+        if (ellipse.rx == ellipse.ry) {
+            let center = CGPointMake(CGFloat(ellipse.cx), CGFloat(ellipse.cy))
+            return UIBezierPath(arcCenter: center, radius: CGFloat(ellipse.rx), startAngle: extent, endAngle: end, clockwise: true)
+        }
+        print("Only circle arc supported for now")
+        return UIBezierPath()
+    }
+
+    private func newCGRect(rect: Rect) -> CGRect {
+        return CGRect(x: CGFloat(rect.x), y: CGFloat(rect.y), width: CGFloat(rect.w), height: CGFloat(rect.h))
+    }
+
+    private func setFill(fill: Fill?, ctx: CGContext?) {
+        if fill != nil {
+            if let color = fill as? Color {
+                let red = CGFloat(Double(color.r()) / 255.0);
+                let green = CGFloat(Double(color.g()) / 255.0);
+                let blue = CGFloat(Double(color.b()) / 255.0);
+                let alpha = CGFloat(Double(color.a()) / 255.0);
+                CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+                CGContextFillPath(ctx);
+            } else {
+                print("Unsupported fill: \(fill)")
+            }
+        }
+    }
+
+    private func setStroke(stroke: Stroke?, ctx: CGContext?) {
+        if stroke != nil {
+            if let color = stroke!.fill as? Color {
+                let red = CGFloat(Double(color.r()) / 255.0)
+                let green = CGFloat(Double(color.g()) / 255.0)
+                let blue = CGFloat(Double(color.b()) / 255.0)
+                let alpha = CGFloat(Double(color.a()) / 255.0)
+                CGContextSetLineWidth(ctx, CGFloat(stroke!.width))
+                CGContextSetLineJoin(ctx, mapLineJoin(stroke!.join))
+                CGContextSetLineCap(ctx, mapLineCap(stroke!.cap))
+                let dashes = stroke!.dashes
+                if !dashes.isEmpty {
+                    let dashPointer = mapDash(dashes)
+                    CGContextSetLineDash(ctx, 0, dashPointer, dashes.count)
+                    dashPointer.dealloc(dashes.count)
+                }
+                CGContextSetRGBStrokeColor(ctx, red, green, blue, alpha)
+                CGContextStrokePath(ctx)
+            } else {
+                print("Unsupported stroke fill: \(stroke!.fill)")
+            }
+        }
+    }
+
+    private func mapLineJoin(join: LineJoin?) -> CGLineJoin {
+        switch join {
+            case LineJoin.round?: return CGLineJoin.Round
+            case LineJoin.bevel?: return CGLineJoin.Bevel
+            default: return CGLineJoin.Miter
+        }
+    }
+
+    private func mapLineCap(cap: LineCap?) -> CGLineCap {
+        switch cap {
+            case LineCap.round?: return CGLineCap.Round
+            case LineCap.square?: return CGLineCap.Square
+            default: return CGLineCap.Butt
+        }
+    }
+
+    private func mapDash(dashes: [NSNumber]) -> UnsafeMutablePointer<CGFloat> {
+        let p = UnsafeMutablePointer<CGFloat>(calloc(dashes.count, sizeof(CGFloat)))
+        for (index, item) in dashes.enumerate() {
+            p[index] = CGFloat(item)
+        }
+        return p;
     }
 
 }

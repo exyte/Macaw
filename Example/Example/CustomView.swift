a7e8947fb0efff7396174e1e0509347ff97c0db3
Nikita Afonasov <bistrform@gmail.com>
2016-02-11 17:34:17 +0600

    Initial image rendering implementation

--- Example/Example/CustomView.swift
+++ Example/Example/CustomView.swift
@@ -210,6 +210,114 @@ class CustomView: MacawView {
             pos: Transform().move(50, my: 520)
         )
         
+        let image1 = Image(src: "../sweet.png",
+            xAlign: Align.min,
+            yAlign: Align.min,
+            aspectRatio: AspectRatio.slice,
+            w: 100,
+            h: 100,
+            pos: Transform().move(100, my: 520)
+        )
+        
+        let leftImage1 = Line(x1: 100, y1: 520, x2: 100, y2: 620)
+        let l11 = Shape(
+            form: leftImage1,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let topImage1 = Line(x1: 100, y1: 520, x2: 200, y2: 520)
+        let l12 = Shape(
+            form: topImage1,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let rightImage1 = Line(x1: 200, y1: 520, x2: 200, y2: 620)
+        let l13 = Shape(
+            form: rightImage1,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let bottomImage1 = Line(x1: 100, y1: 620, x2: 200, y2: 620)
+        let l14 = Shape(
+            form: bottomImage1,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let image2 = Image(src: "../sweet.png",
+            xAlign: Align.min,
+            yAlign: Align.min,
+            aspectRatio: AspectRatio.meet,
+            w: 100,
+            h: 100,
+            pos: Transform().move(250, my: 520)
+        )
+        
+        let leftImage2 = Line(x1: 250, y1: 520, x2: 250, y2: 620)
+        let l21 = Shape(
+            form: leftImage2,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let topImage2 = Line(x1: 250, y1: 520, x2: 350, y2: 520)
+        let l22 = Shape(
+            form: topImage2,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let rightImage2 = Line(x1: 350, y1: 520, x2: 350, y2: 620)
+        let l23 = Shape(
+            form: rightImage2,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+        
+        let bottomImage2 = Line(x1: 250, y1: 620, x2: 350, y2: 620)
+        let l24 = Shape(
+            form: bottomImage2,
+            stroke: Stroke(
+                fill: Color.black,
+                width: 1,
+                cap: .round,
+                join: .round
+            )
+        )
+
+
+        
         func cloudExample() -> Group {
             func cloud1() -> Path {
                 return Path(segments: [
@@ -341,6 +449,16 @@ class CustomView: MacawView {
                 text5,
                 text6,
                 text7,
+                image1,
+                l11,
+                l12,
+                l13,
+                l14,
+                image2,
+                l21,
+                l22,
+                l23,
+                l24,
                 cloudExample()
             ]
         )
--- Source/views/MacawView.swift
+++ Source/views/MacawView.swift
@@ -23,7 +23,7 @@ public class MacawView: UIView {
     }
 
     override public func drawRect(rect: CGRect) {
-        let ctx = UIGraphicsGetCurrentContext();
+        let ctx = UIGraphicsGetCurrentContext()
         drawNode(node, ctx: ctx)
     }
 
@@ -43,6 +43,8 @@ public class MacawView: UIView {
                 }
             } else if let text = node as? Text {
                 drawText(text, ctx: ctx)
+            } else if let image = node as? Image {
+                drawImage(image, ctx: ctx)
             } else {
                 print("Unsupported node: \(node)")
             }
@@ -105,6 +107,111 @@ public class MacawView: UIView {
             return UIColor(CGColor: mapColor(color))
         }
         return UIColor.blackColor()
+    }
+    
+    private func drawImage(image: Image, ctx: CGContext?) {
+        if let uiimage = UIImage(named: image.src) {
+            let imageSize = uiimage.size
+            var w = CGFloat(image.w)
+            var h = CGFloat(image.h)
+            var rect: CGRect
+            if ((w == 0 || w == imageSize.width) && (h == 0 || h == imageSize.height)) {
+                rect = CGRectMake(0, 0, imageSize.width, imageSize.height)
+            } else {
+                if (w == 0) {
+                    w = imageSize.width * h / imageSize.height
+                } else if (h == 0) {
+                    h = imageSize.height * w / imageSize.width
+                }
+                switch (image.aspectRatio) {
+                case AspectRatio.meet:
+                    rect = calculateMeetAspectRatio(image, size: imageSize)
+                case AspectRatio.slice:
+                    rect = calculateSliceAspectRatio(image, size: imageSize)
+                    CGContextClipToRect(ctx, CGRectMake(0, 0, w, h))
+                default:
+                    rect = CGRectMake(0, 0, w, h)
+                }
+            }
+            uiimage.drawInRect(rect)
+        }
+    }
+
+    
+    private func calculateMeetAspectRatio(image: Image, size: CGSize) -> CGRect {
+        let w = CGFloat(image.w)
+        let h = CGFloat(image.h)
+        // destination and source aspect ratios
+        let destAR = w / h
+        let srcAR = size.width / size.height
+        var resultW = w
+        var resultH = h
+        var destX = CGFloat(0)
+        var destY = CGFloat(0)
+        if (destAR < srcAR) {
+        // fill all available width and scale height
+            resultH = size.height * w / size.width
+        } else {
+        // fill all available height and scale width
+            resultW = size.width * h / size.height
+        }
+        let xalign = image.xAlign
+        switch (xalign) {
+            case Align.min:
+                destX = 0
+            case Align.mid:
+                destX = w / 2 - resultW / 2
+            case Align.max:
+                destX = w - resultW
+        }
+        let yalign = image.yAlign
+        switch (yalign) {
+        case Align.min:
+            destY = 0
+        case Align.mid:
+            destY = h / 2 - resultH / 2
+        case Align.max:
+            destY = h - resultH
+        }
+        return CGRectMake(destX, destY, resultW, resultH)
+    }
+    
+    private func calculateSliceAspectRatio(image: Image, size: CGSize) -> CGRect {
+        let w = CGFloat(image.w)
+        let h = CGFloat(image.h)
+        var srcX = CGFloat(0)
+        var srcY = CGFloat(0)
+        var totalH: CGFloat = 0
+        var totalW: CGFloat = 0
+        // destination and source aspect ratios
+        let destAR = w / h
+        let srcAR = size.width / size.height
+        if (destAR > srcAR) {
+            // fill all available width and scale height
+            totalH = size.height * w / size.width
+            totalW = w
+            switch (image.yAlign) {
+            case Align.min:
+                srcY = 0
+            case Align.mid:
+                srcY = -(totalH / 2 - h / 2)
+            case Align.max:
+                srcY = -(totalH - h)
+            }
+        } else {
+            // fill all available height and scale width
+            totalW = size.width * h / size.height
+            totalH = h
+            switch (image.xAlign) {
+            case Align.min:
+                srcX = 0
+            case Align.mid:
+                srcX = -(totalW / 2 - w / 2)
+            case Align.max:
+                srcX = -(totalW - w)
+            }
+        }
+        return CGRectMake(srcX, srcY, totalW, totalH)
     }
     
     private func setGeometry(locus: Locus, ctx: CGContext?) {
@@ -374,7 +481,7 @@ public class MacawView: UIView {
         if fill != nil {
             if let color = fill as? Color {
                 CGContextSetFillColorWithColor(ctx, mapColor(color))
-                CGContextFillPath(ctx);
+                CGContextFillPath(ctx)
             } else if let gradient = fill as? LinearGradient {
                 var start = CGPointMake(CGFloat(gradient.x1), CGFloat(gradient.y1))
                 var end = CGPointMake(CGFloat(gradient.x2), CGFloat(gradient.y2))
@@ -424,10 +531,10 @@ public class MacawView: UIView {
     }
     
     private func mapColor(color: Color) -> CGColor {
-        let red = CGFloat(Double(color.r()) / 255.0);
-        let green = CGFloat(Double(color.g()) / 255.0);
-        let blue = CGFloat(Double(color.b()) / 255.0);
-        let alpha = CGFloat(Double(color.a()) / 255.0);
+        let red = CGFloat(Double(color.r()) / 255.0)
+        let green = CGFloat(Double(color.g()) / 255.0)
+        let blue = CGFloat(Double(color.b()) / 255.0)
+        let alpha = CGFloat(Double(color.a()) / 255.0)
         return UIColor(red: red, green: green, blue: blue, alpha: alpha).CGColor
     }
 
@@ -452,7 +559,7 @@ public class MacawView: UIView {
         for (index, item) in dashes.enumerate() {
             p[index] = CGFloat(item)
         }
-        return p;
+        return p
     }
 
 }

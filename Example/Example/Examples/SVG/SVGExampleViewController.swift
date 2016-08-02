85ee0cec1a3981df73790b7877ffd6fbf8abdbe7
Victor Sukochev <sukochevvv@gmail.com>
2016-08-02 16:05:39 +0600

    Pod structure refactoring

--- Example/Example/Examples/SVG/SVGExampleViewController.swift
+++ Example/Example/Examples/SVG/SVGExampleViewController.swift
@@ -3,17 +3,19 @@ import Macaw
 
 class SVGExampleView: MacawView {
 
-	required init?(coder aDecoder: NSCoder) {
-		let path = NSBundle.mainBundle().pathForResource("tiger", ofType: "svg")
-		let text = try! String(contentsOfFile: path!, encoding: NSUTF8StringEncoding)
+	/*
+	 required init?(coder aDecoder: NSCoder) {
+	 let path = NSBundle.mainBundle().pathForResource("tiger", ofType: "svg")
+	 let text = try! String(contentsOfFile: path!, encoding: NSUTF8StringEncoding)
 
-		//let transform = Transform().move(150, my: 150).scale(0.8, sy: 0.8)
-		let parser = SVGParser(text)
-		let tigerNode = parser.parse()
+	 //let transform = Transform().move(150, my: 150).scale(0.8, sy: 0.8)
+	 let parser = SVGParser(text)
+	 let tigerNode = parser.parse()
 
-		super.init(node: tigerNode, coder: aDecoder)
-	}
+	 super.init(node: tigerNode, coder: aDecoder)
+	 }
 
+	 */
 	required init?(node: Node?, coder aDecoder: NSCoder) {
 		super.init(node: node, coder: aDecoder)
 	}

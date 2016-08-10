56dfd992ccbe70f26b0114b804f1849e25d41db8
Yuri Strot <ystrot@gmail.com>
2016-08-10 18:38:59 +0700

    Add DropShadow effect model

---
+++ Source/model/draw/DropShadow.swift
@@ -0,0 +1,18 @@
+import Foundation
+import RxSwift
+
+public class DropShadow: Effect  {
+
+	public let radius: Double
+	public let offset: Point
+	public let color: Color
+	public let input: Effect?
+
+	public init(radius: Double = 0, offset: Point, color: Color = .black, input: Effect? = nil) {
+		self.radius = radius
+		self.offset = offset
+		self.color = color
+		self.input = input
+	}
+
+}

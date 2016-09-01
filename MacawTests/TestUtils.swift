0e03d39365c97d7a8112b512928c35e115d2bf8a
Victor Sukochev <sukochevvv@gmail.com>
2016-09-01 16:03:48 +0600

    Svg tests added

--- MacawTests/TestUtils.swift
+++ MacawTests/TestUtils.swift
@@ -3,32 +3,31 @@ import Macaw
 
 class TestUtils {
 
-    class func compareWithReferenceObject(fileName: String, referenceObject: AnyObject) -> Bool {
-        let bundle = NSBundle(forClass: TestUtils().dynamicType)
-        if let path = bundle.pathForResource(fileName, ofType: "svg") {
-            let content = try? NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
-            if let svgString = content as? String {
-                let parser = SVGParser(svgString)
-                let group = parser.parse()
-                let referenceArray = TestUtils.prepareParametersList(Mirror(reflecting: referenceObject))
-                let parametersArray = TestUtils.prepareParametersList(Mirror(reflecting: group))
-                return referenceArray.elementsEqual(parametersArray, isEquivalent: { first, second in
-                    return first.0 == second.0 && first.1 == second.1
-                })
-            }
-        }
-        return false
-    }
+	class func compareWithReferenceObject(fileName: String, referenceObject: AnyObject) -> Bool {
+		let bundle = NSBundle(forClass: TestUtils().dynamicType)
+		if let path = bundle.pathForResource(fileName, ofType: "svg") {
+			let content = try? NSString(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+			if let svgString = content as? String {
+				let group = SVGParser.parse(text: svgString)
+				let referenceArray = TestUtils.prepareParametersList(Mirror(reflecting: referenceObject))
+				let parametersArray = TestUtils.prepareParametersList(Mirror(reflecting: group))
+				return referenceArray.elementsEqual(parametersArray, isEquivalent: { first, second in
+					return first.0 == second.0 && first.1 == second.1
+				})
+			}
+		}
+		return false
+	}
 
-    class func prepareParametersList(mirror: Mirror) -> [(String, String)] {
-        var result: [(String, String)] = []
-        for (_, attribute) in mirror.children.enumerate() {
-            if let label = attribute.label where label == "_value" || label.characters.first != "_" {
-                result.append((label, String(attribute.value)))
-                result.appendContentsOf(prepareParametersList(Mirror(reflecting: attribute.value)))
-            }
-        }
-        return result
-    }
+	class func prepareParametersList(mirror: Mirror) -> [(String, String)] {
+		var result: [(String, String)] = []
+		for (_, attribute) in mirror.children.enumerate() {
+			if let label = attribute.label where label == "_value" || label.characters.first != "_" {
+				result.append((label, String(attribute.value)))
+				result.appendContentsOf(prepareParametersList(Mirror(reflecting: attribute.value)))
+			}
+		}
+		return result
+	}
 
 }

3cb8b0a4d52fb2001a290a97fe2bfe8fefc078c0
Victor Sukochev <sukochevvv@gmail.com>
2016-09-08 16:26:54 +0600

    Animation transform order fix

--- Source/animation/AnimationUtils.swift
+++ Source/animation/AnimationUtils.swift
@@ -10,7 +10,7 @@ class AnimationUtils {
 		var transform = pos
 		var parent = nodesMap.parents(node).first
 		while parent != .None {
-			transform = GeomUtils.concat(t1: transform, t2: parent!.place)
+			transform = GeomUtils.concat(t1: parent!.place, t2: transform)
 			parent = nodesMap.parents(parent!).first
 		}
 

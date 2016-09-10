c00ebecdeecbe6d679138a684e159f9a1963a1fd
Yuri Strot <ystrot@gmail.com>
2016-09-10 18:41:04 +0700

    Add Locus utilities

--- Source/model/geom2d/Locus.swift
+++ Source/model/geom2d/Locus.swift
@@ -11,4 +11,19 @@ public class Locus {
 		return Rect()
 	}
 
+	// GENERATED NOT
+	public func stroke(with with: Stroke) -> Shape {
+		return Shape(form: self, stroke: with)
+	}
+
+	// GENERATED NOT
+	public func fill(with with: Fill) -> Shape {
+		return Shape(form: self, fill: with)
+	}
+
+	// GENERATED NOT
+	public func stroke(fill fill: Fill = Color.black, width: Double = 1, cap: LineCap = .butt, join: LineJoin = .miter, dashes: [Double] = []) -> Shape {
+		return Shape(form: self, stroke: Stroke(fill: fill, width: width, cap: cap, join: join, dashes: dashes))
+	}
+
 }

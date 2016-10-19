061ac5020f2d28ec53b20d3063e12700d0e030df
Victor Sukochev <sukochevvv@gmail.com>
2016-10-19 15:51:56 +0700

    Package fix

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name: "Macaw",
   dependencies: [
         .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(3, 0, 0, prereleaseIdentifiers: ["rc"])),
-        .Package(url: "https://github.com/drmohundro/SWXMLHash.git", majorVersion: 3.0),
+        .Package(url: "https://github.com/drmohundro/SWXMLHash.git", Version(3.0.0)),
     ]
 )
 

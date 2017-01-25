1f3b1103f38af37571481d04dde4732216b181ef
Viktor Sukochev <sukochevvv@gmail.com>
2017-01-25 18:26:59 +0700

    Morphing fixes and tests

--- Example/Example/Examples/Morphing/MorphingView.swift
+++ Example/Example/Examples/Morphing/MorphingView.swift
@@ -14,11 +14,14 @@ class MorphingView: MacawView {
     }
     
     class func newScene() -> Node {
-        let rectangle = Rect(x: 50.0, y: 50.0, w: 100.0, h: 100.0)
-        let circle = Circle(cx: 100.0, cy: 100.0, r: 50.0)
+        let form1 = Rect(x: 100.0, y: 100.0, w: 100.0, h: 100.0)
+        //let form1 = Circle(cx: 150.0, cy: 150.0, r: 50.0)
+        let form2 = Circle(cx: 150.0, cy: 150.0, r: 25.0)
+        //let form1 = Line(x1: 100.0, y1: 100.0, x2: 200.0, y2: 200.0)
+        //let form2 = Line(x1: 100.0, y1: 200.0, x2: 200.0, y2: 100.0)
         
-        let shape = Shape(form: rectangle)
-        shape.formVar.animate(to: circle, during:5.0)
+        let shape = Shape(form: form1)
+        shape.formVar.animate(to: form2, during:5.0)
         
         return shape
     }

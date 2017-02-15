337b58cc837e85c83cfdecfc37e5173e2e7ef7a6
Viktor Sukochev <sukochevvv@gmail.com>
2017-02-15 17:09:25 +0700

    Unused operator removed

--- Source/animation/layer_animation/Extensions/AnimOperators.swift
+++ Source/animation/layer_animation/Extensions/AnimOperators.swift
@@ -1,11 +1,6 @@
 
 import Foundation
 
-infix operator ~ {
-	associativity right
-	precedence 155
-}
-
 public func >> (a: Double, b: Double) -> OpacityAnimationDescription {
 	return OpacityAnimationDescription(valueFunc: { t in
 		return a.interpolate(b, progress: t)

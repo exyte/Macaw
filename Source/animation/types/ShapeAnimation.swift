a2b697a2acdfdb50f0590ad41889078f0a0bb9ee
Viktor Sukochev <sukochevvv@gmail.com>
2017-02-15 17:26:05 +0700

    fillVar animation

--- Source/animation/types/ShapeAnimation.swift
+++ Source/animation/types/ShapeAnimation.swift
@@ -41,15 +41,17 @@ class ShapeAnimation: AnimationImpl<Shape> {
     }
 }
 
-// public typealias StrokeAnimationDescription = AnimationDescription<Stroke?>
-
 public extension AnimatableVariable {
     public func animate<T:Stroke>(from: T? = nil, to: T, during: Double = 1.0, delay: Double = 0.0) {
         var shape = node as! Shape
         
         var safeFrom = from
         if safeFrom == nil {
-            safeFrom =  shape.stroke as! T
+            if let shapeStroke = shape.stroke as? T {
+                safeFrom = shapeStroke
+            } else {
+                safeFrom = Stroke(width: 1.0) as! T
+            }
         }
         
         shape.stroke = safeFrom
@@ -81,6 +83,60 @@ public extension AnimatableVariable {
         let finalShape = Shape(form: shape.form,
                                fill: shape.fill,
                                stroke: to,
+                               place: shape.place,
+                               opaque: shape.opaque,
+                               opacity: shape.opacity,
+                               clip: shape.clip,
+                               effect: shape.effect,
+                               visible: shape.visible,
+                               tag: shape.tag)
+        
+        return ShapeAnimation(animatedNode: shape, finalValue: finalShape, animationDuration: during, delay: delay, autostart: false)
+    }
+}
+
+public extension AnimatableVariable {
+    public func animate<T:Fill>(from: T? = nil, to: T, during: Double = 1.0, delay: Double = 0.0) {
+        var shape = node as! Shape
+        
+        var safeFrom = from
+        if safeFrom == nil {
+            if let shapeFill = shape.fill as? T {
+                safeFrom = shapeFill
+            } else {
+                safeFrom = Color.clear as! T
+            }
+        }
+        
+        shape.fill = safeFrom
+        
+        let finalShape = Shape(form: shape.form,
+                               fill: to,
+                               stroke: shape.stroke,
+                               place: shape.place,
+                               opaque: shape.opaque,
+                               opacity: shape.opacity,
+                               clip: shape.clip,
+                               effect: shape.effect,
+                               visible: shape.visible,
+                               tag: shape.tag)
+        
+        let _ = ShapeAnimation(animatedNode: shape, finalValue: finalShape, animationDuration: during, delay: delay, autostart: true)
+    }
+    
+    public func animation<T:Fill>(from: T? = nil, to: T, during: Double = 1.0, delay: Double = 0.0) -> Animation {
+        var shape = node as! Shape
+        
+        var safeFrom = from
+        if safeFrom == nil {
+            safeFrom =  shape.fill as! T
+        }
+        
+        shape.fill = safeFrom
+        
+        let finalShape = Shape(form: shape.form,
+                               fill: to,
+                               stroke: shape.stroke,
                                place: shape.place,
                                opaque: shape.opaque,
                                opacity: shape.opacity,

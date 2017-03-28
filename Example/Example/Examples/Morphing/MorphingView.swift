776cfc0ff6c14bcde11984ad77e6cb1efc766fda
Viktor Sukochev <sukochevvv@gmail.com>
2017-03-28 14:16:19 +0700

    Revert "Feed logic updated to support mixed content"
    This reverts commit 93ae388b054415dc2084d1dbafb5b259e168621d.

--- Example/Example/Examples/Morphing/MorphingView.swift
+++ Example/Example/Examples/Morphing/MorphingView.swift
@@ -12,35 +12,15 @@ class MorphingView: MacawView {
 
     required init?(coder aDecoder: NSCoder) {
         super.init(node: MorphingView.newScene(), coder: aDecoder)
-        
-        var offset = 0.0
-        
-        weak var someNode: Node?
-        while true {
-            //DispatchQueue.global(qos: .utility).asyncAfter(deadline: .now(), execute: {
-            
-            let someNode = try? SVGParser.parse(path: "tiger")
-            
-                //weak var someNode = SVGParser(
-
-            //})
-            
-            offset += 1
-            if offset >= 20.0 {
-                break
-            }
-        }
     }
 
     class func newScene() -> Node {
-   
-        /*
         let stroke = Stroke(width: 15.0, cap: .round)
 		
 		let contents1 = [
 			Shape(form: Line(x1: 150.0, y1: 150.0, x2: 175.0, y2: 125.0), stroke: stroke),
 			Shape(form: Line(x1: 150.0, y1: 150.0, x2: 225.0, y2: 150.0), stroke: stroke),
-			Shape(form: Line(x1: 150.0, y1: 150.0, x2: 175.0, y2: 175.0), stroke: stroke),
+			Shape(form: Line( x1: 150.0, y1: 150.0, x2: 175.0, y2: 175.0), stroke: stroke),
 		]
 
         let contents2 = [
@@ -56,28 +36,5 @@ class MorphingView: MacawView {
 			switched = !switched
 		}
 		return group
- */
- 
-        /*
-        let forms = try! SVGParser.parse(path: "examples") as! Group
-        let elephant = forms.nodeFor(tag: "elephant")!
-        let hippo = forms.nodeFor(tag: "hippo")!
-        let circle = forms.nodeFor(tag: "circle")!
-        let star  = forms.nodeFor(tag: "star")!
-        
-        let contentGroup = [elephant].group()
-        contentGroup.contentsVar.animation(to: [hippo], during: 3.0).autoreversed().cycle().play()
-        
-        return contentGroup
- */
-        let form1 = Rect(x: 50.0, y: 50.0, w: 200.0, h: 200.0)
-        let form2 = Circle(cx: 150.0, cy: 150.0, r: 100.0)
-        
-        let shape = Shape(form: form1, stroke: Stroke(width: 3.0))
-        let animation = shape.formVar.animation(to: form2, during:1.5, delay: 2.0)
-        animation.autoreversed().cycle().play()
-        
-        return shape
- 
 	}
 }

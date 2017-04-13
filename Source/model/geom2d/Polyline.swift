8ccd0abcacd51e3b30cc2e988c8970022e1ffa53
Viktor Sukochev <sukochevvv@gmail.com>
2017-04-13 11:34:08 +0700

    Polyline bounds fix

--- Source/model/geom2d/Polyline.swift
+++ Source/model/geom2d/Polyline.swift
@@ -7,5 +7,40 @@ open class Polyline: Locus {
 	public init(points: [Double] = []) {
 		self.points = points
 	}
+    
+    
+    override open func bounds() -> Rect {
+        var minX = Double(INT16_MAX)
+        var minY = Double(INT16_MAX)
+        var maxX = Double(INT16_MIN)
+        var maxY = Double(INT16_MIN)
+        
+        var isX = true
+        for point in points {
+            if isX {
+                if minX > point {
+                    minX = point
+                }
+                
+                if maxX < point {
+                    maxX = point
+                }
+            } else {
+                if minY > point {
+                    minY = point
+                }
+                    
+                if maxY < point {
+                    maxY = point
+                }
+            }
+            
+            isX = !isX
+        }
+        
+        return Rect(x: minX, y: minY,
+                    w: maxX - minX,
+                    h: maxY - minY)
+    }
 
 }

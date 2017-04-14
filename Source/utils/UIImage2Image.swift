f2ac082a0fd2f883deae7408b884024187e6fad5
Viktor Sukochev <sukochevvv@gmail.com>
2017-04-14 16:57:35 +0700

    Refactoring

--- Source/utils/UIImage2Image.swift
+++ Source/utils/UIImage2Image.swift
@@ -10,19 +10,28 @@ import UIKit
 
 var imagesMap = [String: UIImage]()
 public extension UIImage {
-    public var image: Image {
-        get {
-            var oldId: String?
-            for key in imagesMap.keys {
-                if self == imagesMap[key] {
-                    oldId = key
-                }
+    public func image( xAlign: Align = .min, yAlign: Align = .min, aspectRatio: AspectRatio = .none, w: Int = 0, h: Int = 0, place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) -> Image {
+        
+        var oldId: String?
+        for key in imagesMap.keys {
+            if self == imagesMap[key] {
+                oldId = key
             }
-            
-            let id = oldId ?? UUID().uuidString
-            imagesMap[id] = self
-            
-            return Image(src: "memory://\(id)")
         }
+        
+        let id = oldId ?? UUID().uuidString
+        imagesMap[id] = self
+        
+        return Image(src: "memory://\(id)",
+            xAlign: xAlign, yAlign: yAlign,
+            aspectRatio: aspectRatio,
+            w: w, h: h,
+            place: place,
+            opaque: opaque,
+            opacity: opacity,
+            clip: clip,
+            effect: effect,
+            visible: visible,
+            tag: tag)
     }
 }

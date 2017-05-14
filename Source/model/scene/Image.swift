c3cc2183118315c465bee6cdc936688e23284644
manindaniil <manindaniil@protonmail.com>
2017-05-15 00:31:28 +0700

    base64 image from svg

--- Source/model/scene/Image.swift
+++ Source/model/scene/Image.swift
@@ -1,71 +1,69 @@
 import UIKit
 
 open class Image: Node {
-
-	open let srcVar: Variable<String>
-	open var src: String {
-		get { return srcVar.value }
-		set(val) { srcVar.value = val }
-	}
-
-	open let xAlignVar: Variable<Align>
-	open var xAlign: Align {
-		get { return xAlignVar.value }
-		set(val) { xAlignVar.value = val }
-	}
-
-	open let yAlignVar: Variable<Align>
-	open var yAlign: Align {
-		get { return yAlignVar.value }
-		set(val) { yAlignVar.value = val }
-	}
-
-	open let aspectRatioVar: Variable<AspectRatio>
-	open var aspectRatio: AspectRatio {
-		get { return aspectRatioVar.value }
-		set(val) { aspectRatioVar.value = val }
-	}
-
-	open let wVar: Variable<Int>
-	open var w: Int {
-		get { return wVar.value }
-		set(val) { wVar.value = val }
-	}
-
-	open let hVar: Variable<Int>
-	open var h: Int {
-		get { return hVar.value }
-		set(val) { hVar.value = val }
-	}
+    
+    open let srcVar: Variable<String>
+    open var src: String {
+        get { return srcVar.value }
+        set(val) { srcVar.value = val }
+    }
+    
+    open let xAlignVar: Variable<Align>
+    open var xAlign: Align {
+        get { return xAlignVar.value }
+        set(val) { xAlignVar.value = val }
+    }
+    
+    open let yAlignVar: Variable<Align>
+    open var yAlign: Align {
+        get { return yAlignVar.value }
+        set(val) { yAlignVar.value = val }
+    }
+    
+    open let aspectRatioVar: Variable<AspectRatio>
+    open var aspectRatio: AspectRatio {
+        get { return aspectRatioVar.value }
+        set(val) { aspectRatioVar.value = val }
+    }
+    
+    open let wVar: Variable<Int>
+    open var w: Int {
+        get { return wVar.value }
+        set(val) { wVar.value = val }
+    }
+    
+    open let hVar: Variable<Int>
+    open var h: Int {
+        get { return hVar.value }
+        set(val) { hVar.value = val }
+    }
     
     private var uiImage: UIImage?
-
-	public init(src: String, xAlign: Align = .min, yAlign: Align = .min, aspectRatio: AspectRatio = .none, w: Int = 0, h: Int = 0, place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) {
-		self.srcVar = Variable<String>(src)
-		self.xAlignVar = Variable<Align>(xAlign)
-		self.yAlignVar = Variable<Align>(yAlign)
-		self.aspectRatioVar = Variable<AspectRatio>(aspectRatio)
-		self.wVar = Variable<Int>(w)
-		self.hVar = Variable<Int>(h)
-		super.init(
-			place: place,
-			opaque: opaque,
-			opacity: opacity,
-			clip: clip,
-			effect: effect,
-			visible: visible,
-			tag: tag
-		)
-	}
+    
+    public init(src: String, xAlign: Align = .min, yAlign: Align = .min, aspectRatio: AspectRatio = .none, w: Int = 0, h: Int = 0, place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) {
+        self.srcVar = Variable<String>(src)
+        self.xAlignVar = Variable<Align>(xAlign)
+        self.yAlignVar = Variable<Align>(yAlign)
+        self.aspectRatioVar = Variable<AspectRatio>(aspectRatio)
+        self.wVar = Variable<Int>(w)
+        self.hVar = Variable<Int>(h)
+        super.init(
+            place: place,
+            opaque: opaque,
+            opacity: opacity,
+            clip: clip,
+            effect: effect,
+            visible: visible,
+            tag: tag
+        )
+    }
     
     override func bounds() -> Rect? {
         if w != 0 && h != 0 {
             return Rect(x: 0.0, y: 0.0, w: Double(w), h: Double(h))
         }
         
-        if uiImage == nil {
-            uiImage = UIImage(named: src)
-        }
+        uiImage = image()
         
         guard let uiImage = uiImage else {
             return .none
@@ -73,8 +71,27 @@ open class Image: Node {
         
         return Rect(x: 0.0, y: 0.0,
                     w: Double(uiImage.size.width),
-                    h: Double(uiImage.size.width))
+                    h: Double(uiImage.size.height))
         
     }
-
+    
+    func image() -> UIImage? {
+        var image: UIImage?
+        
+        if uiImage == nil {
+            image = UIImage(named: src)
+        }
+        
+        if uiImage == nil {
+            if src.hasPrefix("data:image/png;base64,") {
+                src = src.replacingOccurrences(of: "data:image/png;base64,", with: "")
+            }
+            
+            if let decodedData = Data(base64Encoded: src, options: .ignoreUnknownCharacters) {
+                image = UIImage(data: decodedData)
+            }
+        }
+        
+        return image
+    }
 }

5d34445f24e2434ff0dbd65f617affd98c226e26
Viktor Sukochev <sukochevvv@gmail.com>
2017-07-03 14:23:28 +0700

    Better equatable node logic

--- Source/animation/types/animation_generators/Cache/NodeHashable.swift
+++ Source/animation/types/animation_generators/Cache/NodeHashable.swift
@@ -3,12 +3,12 @@
 // TODO: Implement better hash
 
 extension Node: Hashable {
-	public var hashValue: Int {
-
-		return Unmanaged.passUnretained(self).toOpaque().hashValue
-	}
+    public var hashValue: Int {
+        
+        return Unmanaged.passUnretained(self).toOpaque().hashValue
+    }
 }
 
 public func == (lhs: Node, rhs: Node) -> Bool {
-	return Unmanaged.passUnretained(lhs).toOpaque() == Unmanaged.passUnretained(rhs).toOpaque()
+    return lhs === rhs
 }

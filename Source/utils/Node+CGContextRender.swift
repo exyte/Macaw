845bbb99dadf885cd6a7054dbea2321d4254da9e
Simon Corsin <scorsin@snapchat.com>
2017-09-07 10:46:02 -0400

    Added comment + missing import.

--- Source/utils/Node+CGContextRender.swift
+++ Source/utils/Node+CGContextRender.swift
@@ -7,9 +7,16 @@
 //
 
 import Foundation
+import CoreGraphics
 
 public extension Node {
 
+    /**
+     Draw the receiver Node in the given rect and CGContext.
+     You can use that if you don't want to use MacawView
+     or want a way to directly render a Node into a CGBitmapContext
+     for instance.
+     */
     public func draw(in rect: CGRect, context: CGContext, opacity: Double = 1) {
         let renderContext = RenderContext(view: nil)
         renderContext.cgContext = context

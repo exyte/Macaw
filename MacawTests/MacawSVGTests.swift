6925209d88ea18ddfbfa72792126eacfa3f01a28
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2017-10-19 00:08:35 +0700

    Fix clip test

--- MacawTests/MacawSVGTests.swift
+++ MacawTests/MacawSVGTests.swift
@@ -25,10 +25,10 @@ class MacawSVGTests: XCTestCase {
 
     func testClipWithParser() {
         let bundle = Bundle(for: type(of: TestUtils()))
-        let clipReferenceContent = "//<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"  ><defs><clipPath id=\"clipPath1\"><path  d=\"M 20 0m -20 0a 20 20 0 1 0 40 0a 20 20 0 1 0 -40 0M 0 10L 0 45L 100 45L 100 10z M 0 55L 0 90L 100 90L 100 55z \" /></clipPath></defs><g><circle  r=\"50\" cy=\"50\" cx=\"50\"  clip-path=\"url(#clipPath1)\"  fill=\"black\"/></g></svg>"
+        let clipReferenceContent = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\"  ><defs><clipPath id=\"clipPath1\"><path  d=\"M 20 0m -20 0a 20 20 0 1 0 40 0a 20 20 0 1 0 -40 0M 0 10L 0 45L 100 45L 100 10z M 0 55L 0 90L 100 90L 100 55z \" /></clipPath></defs><g><circle  r=\"50\" cy=\"50\" cx=\"50\"  clip-path=\"url(#clipPath1)\"  fill=\"black\"/></g></svg>"
         do {
             let node = try SVGParser.parse(bundle:bundle, path: "clip")
-            XCTAssert(SVGSerializer.serialize(node: node) == clipReferenceContent)
+            XCTAssertEqual(SVGSerializer.serialize(node: node), clipReferenceContent)
         } catch {
             print(error)
         }

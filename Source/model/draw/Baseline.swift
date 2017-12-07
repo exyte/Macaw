f6f1a493fb50ed564fd41ad24686e8a61f5e66c8
Lupurus <32777429+Lupurus@users.noreply.github.com>
2017-12-07 23:38:57 +0100

    Update Baseline.swift

--- Source/model/draw/Baseline.swift
+++ Source/model/draw/Baseline.swift
@@ -1,6 +1,16 @@
 public enum Baseline {
+    /// The text will be placed at its top
     case top
+    /// The text will be placed at the bottom of the characters (expect those like "g", "y", the descending characters)
     case alphabetic
+    /// The text will be placed at its bottom
     case bottom
+    /// The text will be placed in the middle
     case mid
+    /// The text will be placed at its top (but without any additional space coming from the font)
+    case perfectTop
+    /// The text will be placed at the bottom of the characters (expect those like "g", "y", the descending characters) (but without any additional space coming from the font)
+    case perfectAlphabetic
+    /// The text will be placed in the middle (but without any additional space coming from the font)
+    case perfectMid
 }

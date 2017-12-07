ee7b6c1834c27297d00a4fad808e3ab60b1b336c
Lupurus <32777429+Lupurus@users.noreply.github.com>
2017-12-07 23:37:50 +0100

    Update Text.swift

--- Source/model/scene/Text.swift
+++ Source/model/scene/Text.swift
@@ -115,23 +115,23 @@ open class Text: Node {
 
     fileprivate func calculateBaselineOffset(font: MFont) -> Double {
         var baselineOffset = 0.0
-		let uselessTopSpace = Double(font.ascender - font.capHeight)	// calculate the space between the height of the capital letters and the height of the largest ascending character (what would that be?)
-		switch baseline {
-		case Baseline.alphabetic:
-			baselineOffset = font.ascender.doubleValue
-		case Baseline.bottom:
-			baselineOffset = (font.ascender - font.descender).doubleValue
-		case Baseline.mid:
-			baselineOffset = (font.ascender - font.descender).doubleValue / 2.0
-		case Baseline.perfectTop:
-			baselineOffset = uselessTopSpace
-		case Baseline.perfectAlphabetic:
-			baselineOffset = Double(font.ascender) + uselessTopSpace
-		case Baseline.perfectMid:
-			baselineOffset = ((font.ascender - font.descender).doubleValue / 2.0) + uselessTopSpace
-		default:
-			break
-		}
+        let uselessTopSpace = Double(font.ascender - font.capHeight)	// calculate the space between the height of the capital letters and the height of the largest ascending character (what would that be?)
+        switch baseline {
+        case Baseline.alphabetic:
+            baselineOffset = font.ascender.doubleValue
+        case Baseline.bottom:
+            baselineOffset = (font.ascender - font.descender).doubleValue
+        case Baseline.mid:
+            baselineOffset = (font.ascender - font.descender).doubleValue / 2.0
+        case Baseline.perfectTop:
+            baselineOffset = uselessTopSpace
+        case Baseline.perfectAlphabetic:
+            baselineOffset = Double(font.ascender) + uselessTopSpace
+        case Baseline.perfectMid:
+            baselineOffset = ((font.ascender - font.descender).doubleValue / 2.0) + uselessTopSpace
+        default:
+            break
+        }
         return -baselineOffset
     }
 

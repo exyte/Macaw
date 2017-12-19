9c7ed8e68bd4eb8844a66ee0417fe5741c23ec37
Yuri Strot <ystrot@gmail.com>
2017-12-19 12:34:40 +0700

    Fix #249: Node Tap handler breaks after animating

--- Source/model/scene/SceneUtils.swift
+++ Source/model/scene/SceneUtils.swift
@@ -8,7 +8,7 @@
 
 class SceneUtils {
     static func shapeCopy(from: Shape) -> Shape {
-        return  Shape(form: from.form,
+        let shape = Shape(form: from.form,
                       fill: from.fill,
                       stroke: from.stroke,
                       place: from.place,
@@ -18,5 +18,16 @@ class SceneUtils {
                       effect: from.effect,
                       visible: from.visible,
                       tag: from.tag)
+		
+		shape.touchPressedHandlers = from.touchPressedHandlers
+		shape.touchMovedHandlers = from.touchMovedHandlers
+		shape.touchReleasedHandlers = from.touchReleasedHandlers
+		
+		shape.tapHandlers = from.tapHandlers
+		shape.panHandlers = from.panHandlers
+		shape.rotateHandlers = from.rotateHandlers
+		shape.pinchHandlers = from.pinchHandlers
+		
+		return shape
     }
 }

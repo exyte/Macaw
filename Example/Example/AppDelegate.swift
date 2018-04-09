5003c5465489ded95f2e53e07bc1de71669884bb
Igor Zapletnev <igor.zapletnev@gmail.com>
2018-04-09 18:44:22 +0700

    AnimationRestorer removed from example project

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func applicationDidBecomeActive(_ application: UIApplication) {
 		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-		AnimationRestorer.restore()
 	}
 
 	func applicationWillTerminate(_ application: UIApplication) {

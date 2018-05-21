70666bd8dda893a0f7ac5decec97035a600a3466
Yuri Strot <ystrot@gmail.com>
2018-05-21 12:45:30 +0700

    Add transformed locus

---
+++ Source/model/geom2d/TransformedLocus.swift
@@ -0,0 +1,17 @@
+//
+//  TransformedLocus.swift
+//  Macaw
+//
+//  Created by Yuri Strot on 5/21/18.
+//
+
+open class TransformedLocus: Locus {
+    
+    public let locus: Locus
+    public let transform: Transform
+
+    public init(locus: Locus, transform: Transform) {
+        self.locus = locus
+        self.transform = transform
+    }
+}

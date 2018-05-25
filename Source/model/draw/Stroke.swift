0295c0e3dac7065efea77fee2fa85b602297a61b
Alisa Mylnikova <f3dm76@gmail.com>
2018-05-25 14:58:11 +0700

    Change default miter limit value

--- Source/model/draw/Stroke.swift
+++ Source/model/draw/Stroke.swift
@@ -8,7 +8,7 @@ open class Stroke {
     open let dashes: [Double]
     open let offset: Double
 
-    public init(fill: Fill = Color.black, width: Double = 1, cap: LineCap = .butt, join: LineJoin = .miter, miterLimit: Double = 4, dashes: [Double] = [], offset: Double = 0.0) {
+    public init(fill: Fill = Color.black, width: Double = 1, cap: LineCap = .butt, join: LineJoin = .miter, miterLimit: Double = 10, dashes: [Double] = [], offset: Double = 0.0) {
         self.fill = fill
         self.width = width
         self.cap = cap

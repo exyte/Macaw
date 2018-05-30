9f3acacfaa8a8df1643d0f76596ba41e19d7ae67
Eugene Nazarov <e.nazarov@yahoo.com>
2018-05-30 14:50:26 +0200

    Code style

--- Source/utils/UIImageExtensions.swift
+++ Source/utils/UIImageExtensions.swift
@@ -14,30 +14,18 @@ import UIKit
 
 public extension UIImage {
 
-    convenience init?(SVGFile filename: String, bundle: Bundle, maxSize: CGSize) {
-
-//        defer {
-//            UIGraphicsEndImageContext()
-//        }
-
+    convenience init?(SVGFile filename: String, bundle: Bundle, maxSize: Size) {
         guard
             let rootNode = try? SVGParser.parse(bundle: bundle, path: filename),
-            let nodeRect = rootNode.bounds()?.toCG() else { return nil }
+            let nodeRect = rootNode.bounds() else { return nil }
 
-        let horizontalRatio = maxSize.width / nodeRect.width
-        let verticalRatio = maxSize.height / nodeRect.height
+        let horizontalRatio = maxSize.w / nodeRect.w
+        let verticalRatio = maxSize.h / nodeRect.h
 
         let ratio = min(horizontalRatio, verticalRatio)
-        let newSize = CGSize(width: nodeRect.width * ratio, height: nodeRect.height * ratio)
-//
-//        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
-//
-//        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        let rect = Rect(x: 0, y: 0, w: nodeRect.w * ratio, h: nodeRect.h * ratio)
+
         let renderer = RenderUtils.createNodeRenderer(rootNode, animationCache: nil)
-
-        let rect = Rect(x: 0, y: 0, w: Double(newSize.width), h: Double(newSize.height))
-
-
 
         if let cgImage = renderer.renderToImage(bounds: rect, inset: 0)?.cgImage {
             self.init(cgImage: cgImage)

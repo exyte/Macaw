27d07e17a963469748d878accb22deed7c7e75b3
Anton Marunko <antohabanderas@yandex.ru>
2018-06-13 17:13:33 +0700

    Fixes for tests

--- MacawTests/Bounds/SVGBoundsTest.swift
+++ MacawTests/Bounds/SVGBoundsTest.swift
@@ -19,13 +19,13 @@ class SVGBoundsTest: XCTestCase {
         do {
             let node = try SVGParser.parse(bundle:bundle, path: name)
             var testResult = false
-            if let bounds = node.bounds() {
+            let bounds = node.bounds()
 //                print("\n<rect x=\"\(Double(round(100*bounds.x)/100))\" y=\"\(Double(round(100*bounds.y)/100))\" width=\"\(Double(round(100*bounds.w)/100))\" height=\"\(Double(round(100*bounds.h)/100))\" stroke=\"red\" stroke-width=\"1\" fill=\"none\"/>\n")
-                testResult = (Double(round(100*bounds.x)/100) - referenceBounds.x < passingThreshold)
-                testResult = testResult && (Double(round(100*bounds.y)/100) - referenceBounds.y < passingThreshold)
-                testResult = testResult && (Double(round(100*bounds.w)/100) - referenceBounds.w < passingThreshold)
-                testResult = testResult && (Double(round(100*bounds.h)/100) - referenceBounds.h < passingThreshold)
-            }
+            testResult = (Double(round(100*bounds.x)/100) - referenceBounds.x < passingThreshold)
+            testResult = testResult && (Double(round(100*bounds.y)/100) - referenceBounds.y < passingThreshold)
+            testResult = testResult && (Double(round(100*bounds.w)/100) - referenceBounds.w < passingThreshold)
+            testResult = testResult && (Double(round(100*bounds.h)/100) - referenceBounds.h < passingThreshold)
+            
             XCTAssert(testResult)
         } catch {
             print(error)

f4bf461c1f8d656e2f7609c22ad68b49a48c6b70
Alisa Mylnikova <f3dm76@gmail.com>
2019-03-22 13:29:47 +0700

    Fix for macos

--- Source/animation/types/PathAnimation.swift
+++ Source/animation/types/PathAnimation.swift
@@ -9,34 +9,34 @@ import Foundation
 
 class PathAnimation: AnimationImpl<Locus> {
     convenience init(animatedNode: Shape, animationDuration: Double, delay: Double = 0.0, autostart: Bool = false, fps: UInt = 30) {
-        
+
         let interpolationFunc = { (t: Double) -> Locus in
             return animatedNode.form
         }
-        
+
         self.init(animatedNode: animatedNode, valueFunc: interpolationFunc, animationDuration: animationDuration, delay: delay, autostart: autostart, fps: fps)
     }
-    
+
     init(animatedNode: Shape, valueFunc: @escaping (Double) -> Locus, animationDuration: Double, delay: Double = 0.0, autostart: Bool = false, fps: UInt = 30) {
         super.init(observableValue: animatedNode.formVar, valueFunc: valueFunc, animationDuration: animationDuration, delay: delay, fps: fps)
         type = .path
         node = animatedNode
-        
+
         if autostart {
             self.play()
         }
     }
-    
+
     init(animatedNode: Shape, factory: @escaping (() -> ((Double) -> Locus)), animationDuration: Double, delay: Double = 0.0, autostart: Bool = false, fps: UInt = 30) {
         super.init(observableValue: animatedNode.formVar, factory: factory, animationDuration: animationDuration, delay: delay, fps: fps)
         type = .path
         node = animatedNode
-        
+
         if autostart {
             self.play()
         }
     }
-    
+
     // Pause state not available for discreet animation
     override public func pause() {
         stop()
@@ -56,14 +56,14 @@ public extension AnimatableVariable where T: LocusInterpolation {
 public extension AnimatableVariable where T: ContentsInterpolation {
     public func appearanceAnimation(during: Double = 1.0, delay: Double = 0.0) -> Animation {
         let group = node as! Group
-        let shapes = group.contents.flatMap{ $0 as? Shape }
-        var animations = shapes.map{ $0.formVar.appearanceAnimation(during: during, delay: delay) }
-        
-        let groups = group.contents.flatMap{ $0 as? Group }
-        let groupAnimations = groups.map({ $0.contentsVar.appearanceAnimation(during: during, delay: delay)})
-        
+        let shapes = group.contents.flatMap { $0 as? Shape }
+        var animations = shapes.map { $0.formVar.appearanceAnimation(during: during, delay: delay) }
+
+        let groups = group.contents.flatMap { $0 as? Group }
+        let groupAnimations = groups.map({ $0.contentsVar.appearanceAnimation(during: during, delay: delay) })
+
         animations.append(contentsOf: groupAnimations)
-        
+
         return animations.combine(node: node)
     }
 }
--- Source/animation/types/animation_generators/PathAnimationGenerator.swift
+++ Source/animation/types/animation_generators/PathAnimationGenerator.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 #if os(iOS)
-    import UIKit
+import UIKit
 #elseif os(OSX)
-    import AppKit
+import AppKit
 #endif
 
 func addPathAnimation(_ animation: BasicAnimation, _ context: AnimationContext, sceneLayer: CALayer, animationCache: AnimationCache?, completion: @escaping (() -> Void)) {
@@ -18,20 +18,20 @@ func addPathAnimation(_ animation: BasicAnimation, _ context: AnimationContext, 
     guard let shape = animation.node as? Shape, let renderer = animation.nodeRenderer else {
         return
     }
-    
+
     let duration = animation.autoreverses ? animation.getDuration() / 2.0 : animation.getDuration()
 
     guard let layer = animationCache?.layerForNodeRenderer(renderer, context, animation: animation, shouldRenderContent: false) else {
         return
     }
-    
+
     // Creating proper animation
     let generatedAnim = generatePathAnimation(from: 0.0, to: 1.0, duration: duration)
-    
+
     generatedAnim.repeatCount = Float(animation.repeatCount)
     generatedAnim.timingFunction = caTimingFunction(animation.easing)
     generatedAnim.autoreverses = animation.autoreverses
-    
+
     generatedAnim.completion = { finished in
 
         animationCache?.freeLayer(renderer)
@@ -39,20 +39,20 @@ func addPathAnimation(_ animation: BasicAnimation, _ context: AnimationContext, 
         if !animation.cycled && !animation.manualStop {
             animation.completion?()
         }
-        
+
         completion()
     }
-    
-    generatedAnim.progress = { progress in        
+
+    generatedAnim.progress = { progress in
         let t = Double(progress)
-        
+
         animation.progress = t
         animation.onProgressUpdate?(t)
     }
 
     layer.path = RenderUtils.toCGPath(shape.form).copy(using: &layer.renderTransform!)
     layer.setupStrokeAndFill(shape)
-        
+
     layer.add(generatedAnim, forKey: animation.ID)
     animation.removeFunc = { [weak layer] in
         layer?.removeAnimation(forKey: animation.ID)
@@ -60,13 +60,13 @@ func addPathAnimation(_ animation: BasicAnimation, _ context: AnimationContext, 
 }
 
 fileprivate func generatePathAnimation(from: Double, to: Double, duration: Double) -> CAAnimation {
-    
+
     let animation = CABasicAnimation(keyPath: "strokeEnd")
     animation.fromValue = from
     animation.toValue = to
     animation.duration = duration
     animation.fillMode = CAMediaTimingFillMode.forwards
     animation.isRemovedOnCompletion = false
-    
+
     return animation
 }

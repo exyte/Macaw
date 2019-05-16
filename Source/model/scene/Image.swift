b80402fd65a72ed2783eb56ad22abd32b5daf13d
Daniil Manin <manindaniil@protonmail.com>
2019-05-16 15:33:35 +0700

    #528 fix memory leak with Image objects

--- Source/model/scene/Image.swift
+++ Source/model/scene/Image.swift
@@ -64,7 +64,9 @@ open class Image: Node {
             tag: tag
         )
 
-        srcVar.onChange { _ in self.mImage = nil }
+        srcVar.onChange { [weak self] _ in
+            self?.mImage = nil
+        }
     }
 
     public init(image: MImage, xAlign: Align = .min, yAlign: Align = .min, aspectRatio: AspectRatio = .none, w: Int = 0, h: Int = 0, place: Transform = Transform.identity, opaque: Bool = true, opacity: Double = 1, clip: Locus? = nil, effect: Effect? = nil, visible: Bool = true, tag: [String] = []) {
@@ -93,7 +95,9 @@ open class Image: Node {
             tag: tag
         )
 
-        srcVar.onChange { _ in self.mImage = nil }
+        srcVar.onChange { [weak self] _ in
+            self?.mImage = nil
+        }
     }
 
     override open var bounds: Rect? {

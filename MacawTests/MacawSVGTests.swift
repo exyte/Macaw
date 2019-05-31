6cb6104871b3babd64fa979451e593c3d758d6f3
Mark Goldin <mgoldin@eastbanctech.com>
2019-05-31 12:45:08 +0700

    Fix #445 drop isDebug flag and use shouldSaveFaildedTestImage flag

--- MacawTests/MacawSVGTests.swift
+++ MacawTests/MacawSVGTests.swift
@@ -23,12 +23,13 @@ class MacawSVGTests: XCTestCase {
     private let testFolderName = "MacawTestOutputData"
     private let shouldComparePNGImages = true
     private let multipleTestsWillRun = true
+    private let shouldSaveFaildedTestImage = true
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         super.setUp()
         
-        if isDebug {
+        if shouldSaveFaildedTestImage {
             setupTestFolderDirectory()
         }
     }

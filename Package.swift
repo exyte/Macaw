244b981581f9fa55e3a2bdf0b6f43da1751b9823
Lupurus <32777429+Lupurus@users.noreply.github.com>
2019-10-05 23:38:56 +0200

    Update Package.swift
    forgot the SWXML dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,9 @@ let package = Package(
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "Macaw",
-			dependencies: [],
+			dependencies: [
+				    .package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.0")
+				],
 			path: "Source"),
 		.testTarget(
 			name: "MacawTests",
